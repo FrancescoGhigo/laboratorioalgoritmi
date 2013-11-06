c54ca5b7c08274996e626d5c84e71f05b4f4befc
viviff9 <michele.castrovilli@gmail.com>
2013-11-06 05:37:22 -0800

    'Test sync'

---
+++ src/castrovilli/esercizio1/BinarySearchTest.java
@@ -0,0 +1,13 @@
+package castrovilli.esercizio1;
+
+public class BinarySearchTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
